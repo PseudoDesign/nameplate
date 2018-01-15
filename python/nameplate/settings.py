7e529d84948cb9a6b7c8a41bb6530019c73d596e
ams-tech <ams-tech@users.noreply.github.com>
2018-01-15 15:44:00 -0700

    added stylesheet

--- python/nameplate/settings.py
+++ python/nameplate/settings.py
@@ -123,4 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'kiosk/templates'),)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "kiosk/static")
+]
 STATIC_URL = '/static/'
