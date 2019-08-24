8f1b56257bbf644ec618887868700741c40b00ad
Adam Schafer <PseudoDesign@users.noreply.github.com>
2019-08-23 20:32:19 -0600

    docker tests passing

--- python/docker/secrets.py
+++ python/docker/secrets.py
@@ -1,3 +1,8 @@
+import os
+
+secrets_directory = "/run/secrets"
+
+
 def get(secret_name, default_value=None):
     """
     Get a docker secret
@@ -5,4 +10,13 @@ def get(secret_name, default_value=None):
     :param default_value:
     :return:
     """
-    pass
+    secret_file = os.path.join(secrets_directory, secret_name)
+    try:
+        with open(secret_file, 'r') as fpt:
+            value = fpt.read()
+    except IOError:
+        if default_value is not None:
+            value = default_value
+        else:
+            raise ValueError(f"Unable to read secret {secret_name}")
+    return value
